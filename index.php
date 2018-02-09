ac66ae09a34f1a731db97aa1f2d77b846ed00e93
andieswong <andreswong5@gmail.com>
2018-02-08 18:40:21 -0800

    aprendiendo ciclos

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+
+
+    <title>index php cursos</title>
+
+
+</head>
+
+<body>
+<h1><a href="variables.php">Variables</a></h1>
+</body>
+
---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$names = ['andres', 'vicente', 'gonzalez', 'wong'];
+
+foreach ( $names as $name){
+
+   echo $name . '<br>' ;
+
+
+}
+
+$numero = 0 ;
+
+    while ( $numero <= 10 ) {
+
+        echo $numero . '<br>';
+
+        $numero++;
+
+    }
