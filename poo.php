62c6d6b682217156c3dadba64dd2d21f705f12c4
andieswong <andreswong5@gmail.com>
2018-02-09 15:53:00 -0800

    Aprendiendo POO 2.0

--- poo.php
+++ poo.php
@@ -1,10 +1,15 @@
 <?php
 
-class Car
+class Vehicle
 {
     private $owner ;
-    public function move() {
-        echo 'moving<br>';
+    public function __construct($ownername)
+    {
+$this ->owner = $ownername;
+    }
+
+    public function move($cars) {
+        echo $cars . ': moving<br>';
     }
     public function GetOwner() {
 
@@ -20,14 +25,46 @@ class Car
     }
 }
 
+class car extends Vehicle{
+
+    public function move($cars) {
+        echo $cars . ': moving<br>';
+    }
+
+}
+
+class truck extends Vehicle{
+
+    private $type;
+
+    public function __construct($ownername, $type)
+    {
+        $this->type = $type;
+        parent::__construct($ownername);
+    }
+
+    public function move($trucks) {
+        echo $trucks . $this->type . ': moving<br>';
+    }
+
+
+}
+
 echo 'class car<br>';
 
-$car = new Car();
-$car2 = new Car();
-$car->move();
-$car->setOwner('Andres');
-$car2->setOwner('Roberto');
+$car = new Car('andy');
+$car2 = new Car('roberto');
+$truck = new truck('jose', 'Explorer');
+$truck2 = new truck('martin', 'Pickup');
+$car->move('car 1');
+$car2->move('car 2');
+$truck->move('truck1');
+$truck2->move('truck2');
+
 
 echo 'Owner carro 1:' . $car->GetOwner() . '<br>';
-echo 'Owner carro 2:' . $car2->GetOwner();
+echo 'Owner carro 2:' . $car2->GetOwner() . '<br>';
+echo 'Owner truck 1:' . $truck->GetOwner() . '<br>';
+echo 'Owner truck 2:' . $truck2->GetOwner() . '<br>';
+
 
