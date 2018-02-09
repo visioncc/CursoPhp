8fb4aeca4f617dae5f7a75f9641957aa48e34e69
andieswong <andreswong5@gmail.com>
2018-02-09 12:18:36 -0800

    Aprendiendo switch (consejo de roman mejia)

--- variables.php
+++ variables.php
@@ -1,21 +1,45 @@
 <?php
 
 
-$names = ['andres', 'vicente', 'gonzalez', 'wong'];
+//$names = ['andres', 'vicente', 'gonzalez', 'wong'];
+//
+//foreach ( $names as $name){
+//
+//   echo $name . '<br>' ;
+//
+//
+//}
+//
+//$numero = 0 ;
+//
+//    while ( $numero <= 10 ) {
+//
+//        echo $numero . '<br>';
+//
+//        $numero++;
+//
+//    }
 
-foreach ( $names as $name){
 
-   echo $name . '<br>' ;
+
+//
+//$color = 'red';
+//
+//switch ($color) {
+//
+//    case 'black':
+//        echo 'Color is : Black';
+//        break;
+//    case 'blue':
+//        echo 'Color is : Blue';
+//        break;
+//    default:
+//        echo 'other';
+//        break ;
+
 
 
 }
 
-$numero = 0 ;
 
-    while ( $numero <= 10 ) {
-
-        echo $numero . '<br>';
-
-        $numero++;
-
-    }
+?>
